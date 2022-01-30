940f6d27c8600b8e3b10836130d9b9b0f786373d
김준하 <kjhkjs93@naver.com>
2022-01-31 03:39:07 +0900

    add matchingDetail / update findPw, logout

--- frontend-react/src/components/MatchingUpdate.js
+++ frontend-react/src/components/MatchingUpdate.js
@@ -68,7 +68,6 @@ export default function MatchingUpdate(props) {
             variant="outlined"
             multiline
             rows={15}
-            maxRows={20}
             onChange={handleInputContent}
           />
           <br />
