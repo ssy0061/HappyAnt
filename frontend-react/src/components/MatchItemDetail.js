effc98969bcc7dde1af140bd3bf440490c7cc851
kbj <tkrhrrlaqudw@naver.com>
2022-02-11 18:28:51 +0900

    Update study create

--- frontend-react/src/components/MatchItemDetail.js
+++ frontend-react/src/components/MatchItemDetail.js
@@ -7,7 +7,6 @@ export default function MatchItemDetail(props) {
     <div>
       <h1>제목 : {item.title}</h1>
       <h1>스터디이름 : {item.studyName}</h1>
-      <p>카테고리 : {item.category}</p>
       <p>내용 : {item.content}</p>
       {item.state ? <p>상태 : 마감</p> : <p>상태 : 모집중</p>}
     </div>
